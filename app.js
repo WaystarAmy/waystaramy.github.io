1be7ea11777455903f2488f7c9c716ce3695614a
Amy Cox-Middleton <amy.middleton@waystar.com>
2022-10-27 12:56:33 -0500

    Convert to App that has random picker

---
+++ app.js
@@ -0,0 +1,55 @@
+(function ()
+{
+    'use strict';
+
+    angular.module('Randomizer', [])
+    .controller('AdderController', AdderController)
+    .controller('ListerController', ListerController)
+    .controller('RandomlySelectedController', RandomlySelectedController)
+    .service('RandomizerService', RandomizerService);
+
+    AdderController.$inject = ['RandomizerService'];
+    function AdderController(RandomizerService)
+    {
+        this.Item = "";
+        this.AddItem = function(){
+            RandomizerService.AddItem(this.Item);
+            this.Item = "";
+        }
+    };
+    ListerController.$inject = ['RandomizerService'];
+    function ListerController(RandomizerService)
+    {
+        this.Items = RandomizerService.GetItems();
+
+        this.RemoveItem = function(itemIndex)
+        {
+            RandomizerService.RemoveItem(itemIndex);
+        }
+    };
+    RandomlySelectedController.$inject = ['RandomizerService'];
+    function RandomlySelectedController(RandomizerService)
+    {
+        this.Item = "";
+        this.GetRandomItem = function(){
+            this.Item = RandomizerService.PickRandomItem();
+        };
+    };
+    function RandomizerService()
+    {
+        var items = [];
+
+        this.AddItem = function(itemName){
+            if(itemName != "")
+            {
+                items.push(itemName);
+            }
+        };
+        this.RemoveItem =function(itemIndex){items.slice(itemIndex,1);};
+        this.PickRandomItem = function(){
+            var randomIndex = Math.floor(Math.random() * items.length);
+            return items[randomIndex];
+        };
+        this.GetItems = function() { return items;}
+    };
+})();
